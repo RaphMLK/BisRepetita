e066cae80a842624e0b42965dd736329c3122ce1
Mouloukin <rmouloukin@gmail.com>
2025-01-19 22:36:25 +0000

    BP-06 - Add the design of the list tiles

--- lib/pages/players.dart
+++ lib/pages/players.dart
@@ -54,20 +54,79 @@ class _BPPlayersListState extends State<BPPlayersList> {
         padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
         children: <Widget>[
           for (int index = 0; index < _players.length; index += 1)
-            ListTile(
-              key: Key('$index'),
+            DecoratedBox(
+              key: Key(index.toString()),
+              position: DecorationPosition.foreground,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom:
+                      Divider.createBorderSide(context, color: Colors.white),
+                ),
+              ),
+              child: ListTile(
+                key: Key('$index'),
+                tileColor: Colors.transparent,
+                title: Text(
+                    style: GoogleFonts.poppins(
+                        fontSize: 26,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300),
+                    '${_players[index].name}'),
+                contentPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                leading: ReorderableDragStartListener(
+                    index: index,
+                    child: const Icon(color: Colors.white, Icons.reorder)),
+                trailing: Container(
+                  height: 24,
+                  padding: const EdgeInsets.all(0),
+                  decoration: const ShapeDecoration(
+                    shape: CircleBorder(),
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _players.removeAt(index);
+                        });
+                      },
+                      iconSize: 24,
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(color: Color(0xFF7B2D26), Icons.close)),
+                ),
+              ),
+            ),
+          DecoratedBox(
+            key: Key('ajouter'),
+            position: DecorationPosition.foreground,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: Divider.createBorderSide(context, color: Colors.white),
+              ),
+            ),
+            child: ListTile(
+              key: Key('ajouter'),
               tileColor: Colors.transparent,
               title: Text(
                   style: GoogleFonts.poppins(
                       fontSize: 26,
                       color: Colors.white,
                       fontWeight: FontWeight.w300),
-                  '${_players[index].name}'),
-              contentPadding: const EdgeInsets.fromLTRB(0, 0, 16, 0),
-              leading: ReorderableDragStartListener(
-                  index: index,
-                  child: const Icon(color: Colors.white, Icons.reorder)),
-            )
+                  'Ajouter'),
+              contentPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+              leading: IconButton(
+                  onPressed: () {},
+                  iconSize: 24,
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints(),
+                  style: const ButtonStyle(
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  icon: const Icon(color: Colors.white, Icons.add)),
+              onTap: () {
+                print('je souhaite ajouter');
+              },
+            ),
+          )
         ],
         onReorder: (int oldIndex, int newIndex) {
           setState(() {
